736e42d0db7227ee5d5c0e37e0f86922633c0811
Greg Weber <greg@gregweber.info>
2011-06-26 18:36:31 -0700

    enable 3.1 asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,8 @@ module Campus
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    # enable the Rails 3.1 asset pipeline
+    config.assets.enabled = true
   end
 end
