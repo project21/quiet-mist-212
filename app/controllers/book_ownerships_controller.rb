79e5a8795801cd24e38913c6c20fc24c0216eb10
Greg Weber <greg@gregweber.info>
2011-07-16 17:23:07 -0700

    add files

---
+++ app/controllers/book_ownerships_controller.rb
@@ -0,0 +1,26 @@
+class BooksController < ApplicationController
+  respond_to :json
+  
+  def index
+    respond_with current_user.books
+  end
+
+  def edit; end
+
+  #def new; end
+  #def destroy; end
+
+  def create
+    @book = Book.new(params[:book])
+    if @book.save
+      BookOwnership.create!(:user => current_user, :book => @book)
+      render :json => @book, :status => :created,
+    else
+      render :json => @book.errors, :status => :unprocessable_entity
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+end
---
+++ lib/books/api/google.rb
@@ -0,0 +1,3 @@
+
+#'AIzaSyAZhdyUePNyBcicNdROlMTBpsK8pzA8iJA'
+#https://www.googleapis.com/books/v1/volumes?q=
