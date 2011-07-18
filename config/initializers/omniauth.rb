4f479008523e33f49f82c8199c36ad4b6dea0661
Greg Weber <greg@gregweber.info>
2011-07-17 18:02:29 -0700

    add oauth credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,32 @@
+class OauthCredentials < Hash
+  def key_secret(provider)
+    creds = fetch(provider)
+    [creds.fetch(:key), creds.fetch(:secret)]
+  end
+end
+
+OAUTH_CREDENTIALS = OauthCredentials[{
+  :twitter => {
+    :key => 'CdXyvNaaWo5f4wqMjzao6Q',
+    :secret => '6NUf1nu43v3CFLkJsMJIQJvXCqF0h0788shmMYFevw'
+  },
+  :facebook => {
+    :key => '230668783631324',
+    :secret => '8713d21f008e58174a8d2beea91f3394'
+  }
+}]
+
 Rails.application.config.middleware.use OmniAuth::Builder do  
-  provider :twitter, 's3dXXXXXXXX', 'lR23XXXXXXXXXXXXXXXXXX'  
-  provider :facebook, 's3dXXXXXXXX', 'lR23XXXXXXXXXXXXXXXXXX'  
+  provider :twitter,  *OAUTH_CREDENTIALS.key_secret(:twitter)
+  provider :facebook, *OAUTH_CREDENTIALS.key_secret(:facebook)
 end  
+
+if Rails.env.development?
+  OmniAuth.config.full_host = "http://localhost:3000"
+else
+  OmniAuth.config.full_host = "http://campusmachine.com"
+end
+
+# Twitter
+# Access token: # 327804893-p6ACPOHCzIwWwfRPIV8zstkVWJ6cowoRlKnj8klA
+# Access token secret: mUwr6rGkN40nCOHjOzi5aF5LnPAkBQHNmucGVCJXI
