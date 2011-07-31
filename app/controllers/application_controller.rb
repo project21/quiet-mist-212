33127a6ca7a2de921f065a2d22183ff0d1b4617a
Greg Weber <greg@gregweber.info>
2011-07-30 19:49:58 -0700

    taken classes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,14 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate_user! 
   protect_from_forgery
   
+protected
   def registered!
     unless current_user.registered
       redirect_to '/home/welcome'
     end
+  end
+
+  def to_boolean param
+    param.present? and (not ["false","0"].include? param)
   end
 end
