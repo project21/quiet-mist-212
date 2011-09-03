78fcad24b3c20cfc56c00fb1cf1814f226f298af
David mbura <davidm1921@yahoo.com>
2011-09-03 13:11:03 -0700

    configure for amazon s3

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -8,8 +8,11 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::ImageScience
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.development?
+    storage :file
+  else
+    storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
