4f825d10a7061f4e805508ed6c3fc2e2e5ec3763
Greg Weber <greg@gregweber.info>
2011-09-12 18:50:31 -0700

    switch to factory_girl

--- Gemfile
+++ Gemfile
@@ -51,8 +51,7 @@ group :test do
   gem 'rspec-rails'
   gem 'capybara'
   gem 'database_cleaner'
-  # TODO: switch to latest factory girl
-  gem 'machinist', '>= 2.0.0.beta2'
   #gem 'factory_girl_rails'
+  gem 'factory_girl', '~>2.0'
   gem 'rr'
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe Post do
   it "creates duplicate posts for each class" do
-    u = User.make!
-    course_ids = (1..3).map {|i| UserCourse.make!(:user => u, :user_id => u.id).course_id }
+    u = create(:user)
+    course_ids = (1..3).map {|i| create(:user_course, :user => u, :user_id => u.id).course_id }
     lambda {
       make_post! :user_id => u.id, :course_ids => course_ids
     }.should change(Post, :count).by(3)
@@ -17,7 +17,7 @@ describe Post do
       course_id = @p.course_id
       @p.course_id = nil
       @p.save.should be_false
-      @p.course_id = Course.make!.id
+      @p.course_id = create(:course).id
       @p.save.should be_false
       @p.course_id = course_id
       @p.should be_true
