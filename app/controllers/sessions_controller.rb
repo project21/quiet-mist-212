4a097cf97cb49f3d8cb27c06acd7607188103e91
Greg Weber <greg@gregweber.info>
2011-09-14 22:53:58 -0700

    fix typo that caused users to get deleted!

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
-class SessionsController < Devise::RegistrationsController
-  skip_before_filter :authenticate_user!
+class SessionsController < Devise::SessionsController
+  #skip_before_filter :authenticate_user!
   skip_before_filter :user_required
   skip_before_filter :registered!
 end
