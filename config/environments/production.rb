e2881bd52f6badd0a2dab30f23fd8122aaa6a048
Greg Weber <greg@gregweber.info>
2011-09-15 11:41:21 -0700

    try to fix static asset serving

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,8 @@ Campus::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
   config.assets.js_compressor  = :uglifier
 
