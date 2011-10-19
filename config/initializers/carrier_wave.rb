d7b20206fe28171399f7fcc9c690be877831d153
Greg Weber <greg@gregweber.info>
2011-10-19 07:26:09 -0700

    use uploads.campusmachine.com as bucket

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -10,7 +10,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => 'MMtC8ziThG4WFap05xh3zrrFP+xuCAGQvwGHUcaV',       # required
     # :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'campusmachine.com'                     # required
+  config.fog_directory  = 'uploads.campusmachine.com'                     # required
   #config.fog_host       = 'https://assets.example.com'            # optional, defaults to nil
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
