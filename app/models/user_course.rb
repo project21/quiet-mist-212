b565d13aa619ce35def9b8c8ba51c441ea2e1936
Greg Weber <greg@gregweber.info>
2011-10-23 15:09:55 -0700

    add schema to UserCourse

--- app/models/user_course.rb
+++ app/models/user_course.rb
@@ -1,4 +1,8 @@
 class UserCourse < ActiveRecord::Base
+  col :user_id,   as: :integer, null: false
+  col :course_id, as: :integer, null: false
+  col :active,    as: :boolean, null: false
+
   belongs_to :user
   belongs_to :course
 end
