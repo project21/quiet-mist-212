d1d2d4d32ac170a0dbea3611a422b1036975ca09
Greg Weber <greg@gregweber.info>
2011-10-23 15:59:05 -0700

    add schema to book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,11 @@
 class Book < ActiveRecord::Base
+   col :title,      as: :string
+   col :author,     as: :string
+   col :created_at, as: :timestamp
+   col :updated_at, as: :timestamp
+   col :edition,    as: :string
+   col :isbn,       as: :string, null:false
+
   has_many :book_ownerships
   has_many :users, :through => :book_ownerships
 
